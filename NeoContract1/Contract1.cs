1389fae9cdd425ed15cfd69b6da4b68b86ab809e
lschaertel <ludwigschaertel@gmail.com>
2018-02-19 22:23:20 +0100

    first funtions
    first functions to create/delete/query credentials

--- NeoContract1/Contract1.cs
+++ NeoContract1/Contract1.cs
@@ -14,137 +14,173 @@ namespace Neo.SmartContract
             {
                 case "setStorage":
                     return setStorage((string)args[0], (object[])args[1]);
+                case "listStorage":
+                    return listStorage((string)args[0]);
                 case "getStorage":
                     return getStorage((string)args[0], (object[])args[1]);
-                case "register":
-                    return Register((string)args[0], (byte[])args[2]);
-                default:
+                case "deleteStorage":
+                    return deleteStorage((string)args[0], (object[])args[1]);
+                //case "rearrangeStorage":
+                    //return rearrangeStorage((byte[])args[0], (byte[])args[1]);
+                default:    
                     return false;
             }
         }
-
+        //Adds a new Domain-Credencial to the given public key and relevant parameters
         private static Boolean setStorage(string pubKey, object[] args)
         {
             byte[] ownerAddress = pubKey.AsByteArray();
             byte[] domain = (byte[])args[0];
             byte[] username = (byte[])args[1];
             byte[] password = (byte[])args[2];
-            //byte[] encryptionType = (byte[])args[3];
+            //byte[] encryptionType = (byte[])args[3]; //ToDo: enable encryption - EncryptionTypes: None/Password/Privatekey (Hash?))
+            //byte[] encryption = (byte[])args[4];
 
-            if (!checkExistingDomain(ownerAddress, domain))
+            byte[] amountDomains = Storage.Get(Storage.CurrentContext, ownerAddress);
+            Runtime.Notify(amountDomains);
+            if (amountDomains == null)
             {
-                byte[] domainAddress = ownerAddress.Concat("domain".AsByteArray());
-                byte[] usernameAddress = domainAddress.Concat(domain.Concat("username".AsByteArray()));
-                byte[] passwordAddress = domainAddress.Concat(domain.Concat("password".AsByteArray()));
-                byte[] encryptionTypeAddress = domainAddress.Concat(domain.Concat("encryption".AsByteArray()));
-
-                Runtime.Notify("SetStorage() pubKey", pubKey);
-                Runtime.Notify("SetStorage() args[0] - domain", args[0]);
-                Runtime.Notify("SetStorage() args[1] - username", args[1]);
-                Runtime.Notify("SetStorage() args[2] - password", args[2]);
-                //Runtime.Notify("SetStorage() args[3] - encyptionType", args[3]);
-
-                Storage.Put(Storage.CurrentContext, domainAddress, domain);
-                Storage.Put(Storage.CurrentContext, usernameAddress, username);
-                Storage.Put(Storage.CurrentContext, passwordAddress, password);
-                //Storage.Put(Storage.CurrentContext, encryptionTypeAddress, encryptionType);
-
-                Runtime.Notify(domainAddress);
-                Runtime.Notify(usernameAddress);
-                Runtime.Notify(passwordAddress);
-
-                updateAddressList(ownerAddress, domain);
+                Storage.Put(Storage.CurrentContext, ownerAddress, 49);
+                amountDomains = Storage.Get(Storage.CurrentContext, ownerAddress);
             }
-            return true;
-        }
-        private static Boolean getStorage(string pubKey, object[] args)
-        {
-            byte[] ownerAddress = pubKey.AsByteArray();
-            byte[] domain = (byte[])args[0];
-            Runtime.Notify(ownerAddress);
-            Runtime.Notify(domain);
-            byte[] storageLocation = ownerAddress.Concat("domain".AsByteArray().Concat(domain));
-            Runtime.Notify(storageLocation);
-            var domainAddress = Storage.Get(Storage.CurrentContext, storageLocation);
-            Runtime.Notify(domainAddress);
-
-            byte[] usernameAddress = storageLocation.Concat("username".AsByteArray());
-            byte[] passwordAddress = storageLocation.Concat("password".AsByteArray());
-
-            var result1 = Storage.Get(Storage.CurrentContext, usernameAddress);
-            Runtime.Notify(result1);
-
-            Runtime.Notify(Storage.Get(Storage.CurrentContext, usernameAddress));
-            Runtime.Notify(Storage.Get(Storage.CurrentContext, passwordAddress));
-
-            //var storage = Storage.Get(Storage.CurrentContext, )
-            //Runtime.Notify(storage);
-
-            return true;
-        }
-        private static Boolean updateAddressList(byte[] domainAddress, byte[] domain)
-        {
-            var storageValue = Storage.Get(Storage.CurrentContext, domainAddress);
-            Runtime.Notify("updateAddressList() storageValueBefore", storageValue);
-            storageValue = storageValue.Concat(domain.Concat(",".AsByteArray()));
-            Runtime.Notify("updateAddressList() storageValueAfter", storageValue);
-            Storage.Put(Storage.CurrentContext, domainAddress, storageValue);
-
-            return true;
-        }
-        //Checks if the new Domains already exists under the PublicKey-Address
-        private static Boolean checkExistingDomain(byte[] domainAddress, byte[] domain)
-        {
-            var storageValue = Storage.Get(Storage.CurrentContext, domainAddress);
-            return Contains(storageValue, domain);
-        }
-
-        private static byte[] GetStorage(string pubKey)
-        {
-            return Storage.Get(Storage.CurrentContext, pubKey);
-        }
-        
-        private static bool Contains(byte[] self, byte[] candidate)
-        {
-            if (IsEmptyLocate(self, candidate))
-                return false;
-
-            for (int i = 0; i < self.Length; i++)
+            else
             {
-                if (IsMatch(self, i, candidate))
-                    return true;
+                amountDomains = (amountDomains.AsBigInteger() + 1).AsByteArray();
+            }          
+            
+            byte[] domainNo = ownerAddress.Concat(amountDomains);
+            byte[] domainName = ownerAddress.Concat(domain);
+
+            if (!checkExisting(domainName))
+            {
+                byte[] domainUsername = domainNo.Concat("username".AsByteArray());
+                byte[] domainPassword = domainNo.Concat("password".AsByteArray());
+                byte[] encryptionType = domainNo.Concat("encryptiontype".AsByteArray());
+                byte[] encryptionKey = domainNo.Concat("encryptionkey".AsByteArray());
+
+                Storage.Put(Storage.CurrentContext, domainNo, domain);
+                Storage.Put(Storage.CurrentContext, domainName, domainNo);
+                Storage.Put(Storage.CurrentContext, domainUsername, username);
+                Storage.Put(Storage.CurrentContext, domainPassword, password);
+                Storage.Put(Storage.CurrentContext, ownerAddress, amountDomains);
+
+                Runtime.Notify("ownerAddress", ownerAddress);
+                Runtime.Notify("domainNo: ", domainNo);
+                Runtime.Notify("domainName: ", domainName);
+                Runtime.Notify("domainUsername: ", domainUsername);
+                Runtime.Notify("domainPassword", domainPassword);
+
+                return true;
             }
 
             return false;
         }
-        
-        private static bool IsMatch(byte[] array, int position, byte[] candidate)
+        //Returns a container with every added domain to the given PublicKey
+        private static object[] listStorage(string pubKey)
         {
-            if (candidate.Length > (array.Length - position))
-                return false;
+            int counterResults = 0;
+            byte[] ownerAddress = pubKey.AsByteArray();
+            BigInteger amountDomains = Storage.Get(Storage.CurrentContext, ownerAddress).AsBigInteger();
+            object[] results = new object[(int)amountDomains - 48];
+            for (BigInteger i = 49; i <= amountDomains;)
+            {
+                byte[] domainNo = ownerAddress.Concat(i.AsByteArray());
+                Runtime.Notify(domainNo);
 
-            for (int i = 0; i < candidate.Length; i++)
-                if (array[position + i] != candidate[i])
-                    return false;
+                results[counterResults] = Storage.Get(Storage.CurrentContext, domainNo);
+                Runtime.Notify(Storage.Get(Storage.CurrentContext, domainNo));
+
+                counterResults++;
+                i = i + 1;
+            }
+
+            return results;
+        }
+        //Checks if there is an existing entry on that given domainName → [PublicKey]+[domain]
+        private static Boolean checkExisting(byte[] domainName)
+        {
+            Runtime.Notify(domainName);
+            var result = Storage.Get(Storage.CurrentContext, domainName);
+            Runtime.Notify(result);
+            if (result == null) return false;
+            return true;
+        }
+        //Returns a container with the personell data to the given PublicKey and domain
+        private static object getStorage(string pubKey, object[] args)
+        {
+            byte[] ownerAddress = pubKey.AsByteArray();
+            byte[] domain = (byte[])args[0];
+            byte[] domainNo = Storage.Get(Storage.CurrentContext, ownerAddress.Concat(domain));
+            if (domainNo == null) return false;
+
+            object[] results = new object[3];
+            results[0] = domain;
+            results[1] = Storage.Get(Storage.CurrentContext, domainNo.Concat("username".AsByteArray()));
+            results[2] = Storage.Get(Storage.CurrentContext, domainNo.Concat("password".AsByteArray()));
+
+            return results;
+        }
+        private static Boolean deleteStorage(string pubKey, object[] args)
+        {
+            byte[] ownerAddress = pubKey.AsByteArray();
+            byte[] domain = (byte[])args[0];
+            byte[] domainNo = Storage.Get(Storage.CurrentContext, ownerAddress.Concat(domain));
+            if (domainNo == null) return false;
+
+            byte[] domainUsername = domainNo.Concat("username".AsByteArray());
+            byte[] domainPassword = domainNo.Concat("password".AsByteArray());
+            byte[] encryptionType = domainNo.Concat("encryptiontype".AsByteArray());
+            byte[] encryptionKey = domainNo.Concat("encryptionkey".AsByteArray());
+
+            Storage.Delete(Storage.CurrentContext, domainNo);
+            Storage.Delete(Storage.CurrentContext, ownerAddress.Concat(domain));
+            Storage.Delete(Storage.CurrentContext, domainUsername);
+            Storage.Delete(Storage.CurrentContext, domainPassword);
+            //Storage.Delete(Storage.CurrentContext, encryptionType);
+            //Storage.Delete(Storage.CurrentContext, encryptionKey);
+
+            Storage.Put(Storage.CurrentContext, ownerAddress, (Storage.Get(Storage.CurrentContext, ownerAddress).AsBigInteger() - 1));
+            rearrangeStorage(ownerAddress, domainNo, domain);
 
             return true;
         }
-        
-        private static bool IsEmptyLocate(byte[] array, byte[] candidate)
+        //ToDo Rearrange correctly 
+        private static Boolean rearrangeStorage(byte[] ownerAddress, byte[] deletedDomainNo, byte[] domainDeleted)
         {
-            return array == null
-                    || candidate == null
-                    || array.Length == 0
-                    || candidate.Length == 0
-                    || candidate.Length > array.Length;
-        }
-        
-        private static bool Register(string domain, byte[] owner)
-        {
-            if (!Runtime.CheckWitness(owner)) return false;
-            byte[] value = Storage.Get(Storage.CurrentContext, domain);
-            if (value != null) return false;
-            Storage.Put(Storage.CurrentContext, domain, owner);
+            var amountDomains = Storage.Get(Storage.CurrentContext, ownerAddress).AsBigInteger();
+            var noDeleted = (BigInteger)deletedDomainNo[deletedDomainNo.Length - 1];
+            
+            if(amountDomains >= noDeleted)
+            {
+                byte[] domainNo = Storage.Get(Storage.CurrentContext, ownerAddress.Concat((amountDomains + 1).AsByteArray()));
+                byte[] domain = Storage.Get(Storage.CurrentContext, domainNo);
+                byte[] domainUsername = domainNo.Concat("username".AsByteArray());
+                byte[] domainPassword = domainNo.Concat("password".AsByteArray());
+                byte[] encryptionType = domainNo.Concat("encryptiontype".AsByteArray());
+                byte[] encryptionKey = domainNo.Concat("encryptionkey".AsByteArray());
+
+                byte[] domainUsernameDeleted = deletedDomainNo.Concat("username".AsByteArray());
+                byte[] domainPasswordDeleted = deletedDomainNo.Concat("password".AsByteArray());
+                byte[] encryptionTypeDeleted = deletedDomainNo.Concat("encryptiontype".AsByteArray());
+                byte[] encryptionKeyDeleted = deletedDomainNo.Concat("encryptionkey".AsByteArray());
+
+                Storage.Put(Storage.CurrentContext, deletedDomainNo, domain);
+                Storage.Put(Storage.CurrentContext, ownerAddress.Concat(domain), domainNo);
+                Storage.Put(Storage.CurrentContext, domainUsernameDeleted, domainUsername);
+
+            }
+
+            byte[] test1 = "AGbvNPxAfUC11yGZWr6LENDPDamRTNWvde1".AsByteArray();
+            byte[] test2 = "AGbvNPxAfUC11yGZWr6LENDPDamRTNWvde2".AsByteArray();
+
+            Runtime.Notify(test1);
+            Runtime.Notify(test2);
+            Runtime.Notify(test1.Length);
+            var test = test2[test2.Length - 1];
+            Runtime.Notify(test);
+
+            //if (deletedDomainNo == ownerAddress.Concat(amountDomains)) ;
+
             return true;
         }
     }
